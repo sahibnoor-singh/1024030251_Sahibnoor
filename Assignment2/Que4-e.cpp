1b66dcab39918124130b527e936316ac25eacae8
Sahibnoor Singh <76655150+sahibnoor-singh@users.noreply.github.com>
2025-10-31 10:03:24 +0530

    q4

--- Assignment2/Que4-e.cpp
+++ Assignment2/Que4-e.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include <string>
 using namespace std;
-void lowerToUpper(string &s) {
-    for (int i=0;i<s.size();i++) {
-        if ((int)s[i]>=97 && (int)s[i]<=122) {
-            s[i]=(char)((int)s[i]-32);
+void upperToLower(string &s) {
+    for (int i = 0; i < s.size(); i++) {
+        if ((int)s[i] >= 65 && (int)s[i] <= 90) {
+            s[i] = (char)((int)s[i] + 32);
         }
     }
     return;
 }
 int main() {
     string s="abcdefABCDEF";
-    lowerToUpper(s);
+    upperToLower(s);
     cout<<s;
     return 0;
 }
